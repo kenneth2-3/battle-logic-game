bc19e3a1d2c34ae03d73c7508d60b9e4ee76ba45
Kenneth Adanma <deborah2020.kenneth@gmail.com>
2025-04-30 15:04:00 +0100

    Add a terminal menu to end or continue playing the game

--- run.py
+++ run.py
@@ -1,4 +1,21 @@
 from game import start_game
+from simple_term_menu import TerminalMenu
+
+
+def main():
+    while True:
+        start_game()
+        options = ["Play Again", "Exit"]
+        terminal_menu = TerminalMenu(
+            options,
+            title="What would you like to do next?"
+        )
+        choice = terminal_menu.show()
+
+        if options[choice] == "Exit":
+            print("Thanks for playing Battleships! Goodbye.")
+            break
+
 
 if __name__ == "__main__":
-    start_game()
+    main()
